3c7095f36cb931c4c1641b172c32e7393c62205b
akhanna75 <akhanna75@gmail.com>
2013-07-08 01:21:15 +0000

    Changed welcome message

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('This is Hello World from Amit Khanna!');
 });
 
 var port = process.env.PORT || 5000;
